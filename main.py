0360706984128705055425c7a979be0c89626be4
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-22 13:50:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+ang = float(input("Digite o angulo: "))
+
+seno = math.sin(math.radians(ang))
+coss = math.cos(math.radians(ang))
+tang = math.tan(math.radians(ang))
+
+print (f"Seno é {seno:.2f}\nConseno é:{coss:.2f}\nTangente é: {tang:.2f}")
